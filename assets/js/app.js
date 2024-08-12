0195c662ce54dcba7f2108b4df242b088b57f3c3
Apple <sndpjaat008@gmail.com>
2024-08-12 12:21:04 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const menuicon = document.querySelector('.menuicon');
+let body = document.body;
+const menu = document.querySelector('.menu');
+
+menuicon.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    if (menu.classList.contains('active')) {
+        body.style.overflow = 'hidden';
+    } else {
+        body.style.overflow = 'auto';
+    }
+});
