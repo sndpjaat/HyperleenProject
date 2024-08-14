f464162873972b3dd889122addcb0f704c77d4c8
Apple <sndpjaat008@gmail.com>
2024-08-14 14:30:18 +0530

    adjust cross icon

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,20 @@ links.forEach(link => {
         body.style.overflow = 'auto'; 
     });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".navlink");
+  
+    navLinks.forEach(link => {
+      link.addEventListener("click", function(event) {
+        event.stopPropagation();
+
+        navLinks.forEach(link => link.classList.remove("active"));
+        this.classList.add("active");
+      });
+    });
+  
+    document.addEventListener("click", function() {
+        navLinks.forEach(link => link.classList.remove("active"));
+    });
+});
